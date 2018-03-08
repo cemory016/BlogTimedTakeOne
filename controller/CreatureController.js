4c128b1d0875f2279a067b2b5c55a91b4283cad8
Carmen <cemory016@gmail.com>
2018-03-08 18:31:52 -0500

    Add a db directory and create a Mongoose model for Creatures

---
+++ controller/CreatureController.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const Creature = require ('../db/schema')
+const router = express.Router({mergeParams: true})
+
+router.get('/', (req, res)=> {
+    })
+get
+put
+post
+delete
---
+++ db/seeds.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+
+const {Creature}  = require('./schema')
+mongoose.connect(process.env.MONGODB_URI)
+
+
+
+
+const db = mongoose.connection
+// using async/await
+const saved = async () => {
+  await Creature.remove()
+  const luke = new Creature({name: 'Luke', description: 'Jedi'})
+  await luke.save()
+  const darth = new Creature({name: 'Darth Vader', description: 'Father of Luke'})
+  await darth.save()
+  db.close()
+}
+
+saved()
